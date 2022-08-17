a58381ffc374b29bf6f4e9da14d327dea4fde20f
SongJunEon <junun24@gmail.com>
2022-08-17 17:15:42 +0900

    페이지 이동 및 소셜  체크박스

---
+++ JS/MovePage.js
@@ -0,0 +1,12 @@
+var script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+        $(".HeaderLogo").click(function(){document.location.href='http://127.0.0.1:5500/html/Home.html'})
+        $(".MusicTalkBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/MusicTalk.html'})
+        $(".PlayListBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/PlayList.html'})
+        $(".MyPageBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/MyPage.html'})
+        $(".LoginBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/Login.html'})
+        $(".SignUpBtn").click(function(){document.location.href='http://127.0.0.1:5500/html/SignUp.html'})
